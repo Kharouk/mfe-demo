0736c1a11a2f1fdbe802e784eb6ba4d2cad0b3b0
Alex Kharouk <akharoukpp@gmail.com>
2021-09-21 06:49:08 +0100

    add container micro-frontend

--- products/src/index.js
+++ products/src/index.js
@@ -2,14 +2,11 @@ import { commerce } from 'faker'
 
 let products = ''
 
-let bodyTag = document.querySelector('.products')
-console.log(bodyTag)
+let productTag = document.querySelector('.products')
 
 for (let i = 0; i < 6; i++) {
   const name = commerce.productName()
   products += `<div>${name}</div>`
 }
 
-bodyTag.innerHTML = products
-
-console.log({products})
+productTag.innerHTML = products
