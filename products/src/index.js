77772794f71a5b318be7d2a43bdf7b26084c8104
Alex Kharouk <akharoukpp@gmail.com>
2021-09-21 06:42:09 +0100

    live reload

--- products/src/index.js
+++ products/src/index.js
@@ -5,7 +5,7 @@ let products = ''
 let bodyTag = document.querySelector('.products')
 console.log(bodyTag)
 
-for (let i = 0; i < 3; i++) {
+for (let i = 0; i < 6; i++) {
   const name = commerce.productName()
   products += `<div>${name}</div>`
 }
