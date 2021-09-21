582888882f67b22c5b9f1d1a160ac2e9aec7799f
Alex Kharouk <akharoukpp@gmail.com>
2021-09-21 06:41:40 +0100

    display fake products on page

--- products/src/index.js
+++ products/src/index.js
@@ -2,9 +2,14 @@ import { commerce } from 'faker'
 
 let products = ''
 
+let bodyTag = document.querySelector('.products')
+console.log(bodyTag)
+
 for (let i = 0; i < 3; i++) {
   const name = commerce.productName()
   products += `<div>${name}</div>`
 }
+
+bodyTag.innerHTML = products
 
 console.log({products})
