b5a0d780450da50a529f0175757ed5fec4e0ab1e
Alex Kharouk <akharoukpp@gmail.com>
2021-09-22 10:26:48 +0100

    import cart into HOST container

--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -1,4 +1,5 @@
-import 'products/ProductsIndex'
-import 'products/test' // set up the alias in products/webpack.config.js
+import 'products/ProductsIndex';
+import 'products/test'; // set up the alias in products/webpack.config.js
+import 'cart/CartShow';
 
-console.log('Imported products/ProductsIndex, now in bootstrap.js')
+console.log('Imported products and cart, now in bootstrap.js');
