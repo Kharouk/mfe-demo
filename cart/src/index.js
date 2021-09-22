f9de5a48356b76d20e6a880af69d453b6302a330
Alex Kharouk <akharoukpp@gmail.com>
2021-09-22 15:03:14 +0100

    add comment on index files in frontend apps

--- cart/src/index.js
+++ cart/src/index.js
@@ -1,3 +1,5 @@
 import('./bootstrap');
 // using import() as a function loads asynchronously, so it allows us to use
 // modules like faker independently from the container.
+
+// giving webpack time to fetch the packages needed to run bootstrap
