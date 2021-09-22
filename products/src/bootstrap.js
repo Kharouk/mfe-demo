28556c9d839cd233e54d91e6bcbacfb64884b5e5
Alex Kharouk <akharoukpp@gmail.com>
2021-09-22 17:21:44 +0100

    25-6: Set up dynamic mounting of sub-projects as we can not guarantee div classnames

--- products/src/bootstrap.js
+++ products/src/bootstrap.js
@@ -1,12 +1,36 @@
 import { commerce } from 'faker';
 
-let products = '';
+const mountProduct = (element) => {
+  let products = '';
 
-let productTag = document.querySelector('.products');
+  // let productTag = document.querySelector('.products');
 
-for (let i = 0; i < 6; i++) {
-  const name = commerce.productName();
-  products += `<div>${name}</div>`;
+  for (let i = 0; i < 6; i++) {
+    const name = commerce.productName();
+    products += `<div>${name}</div>`;
+  }
+
+  // ala ReactDOM.render(<App />, element);
+  element.innerHTML = products;
+};
+
+/*
+ * Assuming that the container 'team' knows of us (products) attaching the code to a
+ * tag with class 'products'
+ *
+ * Context/Situation #1
+ * - We are running the file isolated from production
+ *    - therefore we use our local index.html file
+ *    - we are sure we will have a div with class 'products'
+ *    - we want to immediately render our app into that element.
+ */
+if (process.env.NODE_ENV === '') {
 }
+/*
+ * Context/Situation #2
+ * - We are running this sub-project/file in development/production WITHIN container (HOST)
+ *    - No guarantee that there will be a div with class 'products'
+ *    - we do not want to immediately render this app
+ */
 
-productTag.innerHTML = products;
+export { mountProduct };
