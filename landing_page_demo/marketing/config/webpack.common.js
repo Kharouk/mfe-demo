f363167f7d3070f3223179d630ac60a5a85f6f97
Alex Kharouk <akharoukpp@gmail.com>
2021-09-23 06:11:36 +0100

    35. create webpack fields for marketing dir

---
+++ landing_page_demo/marketing/config/webpack.common.js
@@ -0,0 +1,18 @@
+module.exports = {
+  module: {
+    rules: [
+      // define loaders
+      {
+        test: /\.m?js$/, // babel processes .mjs & .js files
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env'],
+            plugins: ['@babel/plugin-transform-runtime'],
+          },
+        },
+      },
+    ],
+  },
+};
