c18b711848446da0cf3078f2626a5e91324a5909
Alex Kharouk <akharoukpp@gmail.com>
2021-09-23 16:57:15 +0100

    get marketing app showing in container!

--- landing_page_demo/container/src/App.js
+++ landing_page_demo/container/src/App.js
@@ -1,10 +1,6 @@
 import React from 'react';
-import { mount as marketingMount } from 'marketing/App';
+import MarketingApp from './components/MarketingApp';
 
 export default () => {
-  return (
-    <div>
-      <h1>Welcome to your SaaSy3 app.</h1>
-    </div>
-  );
+  return <MarketingApp />;
 };
