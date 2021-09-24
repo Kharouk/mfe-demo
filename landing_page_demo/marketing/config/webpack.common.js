6d029d0d857b458cf1fcd6c28379f27e8b7115aa
Alex Kharouk <akharoukpp@gmail.com>
2021-09-24 06:53:34 +0100

    set up webpack production config for the marketing app

--- landing_page_demo/marketing/config/webpack.common.js
+++ landing_page_demo/marketing/config/webpack.common.js
@@ -1,3 +1,5 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   module: {
     rules: [
@@ -15,4 +17,9 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
 };
