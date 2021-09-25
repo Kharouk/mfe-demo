ef1f040b55eea58ea16cac7824340b7ce0155d1a
Alex Kharouk <akharoukpp@gmail.com>
2021-09-25 07:31:49 +0100

    add production domain secret

--- landing_page_demo/container/config/webpack.prod.js
+++ landing_page_demo/container/config/webpack.prod.js
@@ -16,8 +16,8 @@ const prodConfig = {
     new ModuleFederationPlugin({
       name: 'container',
       remotes: {
-        marketing: `marketing@${domain}/marketing/remoteEntry.js`,
-        auth: `auth@${domain}/auth/remoteEntry.js`,
+        marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`,
+        // auth: `auth@${domain}/auth/latest/remoteEntry.js`,
       },
       shared: packageJson.dependencies,
     }),
