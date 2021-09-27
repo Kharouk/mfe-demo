228371d73d11e86f668ab72df7f1661e8ea6d394
Alex Kharouk <akharoukpp@gmail.com>
2021-09-27 06:02:01 +0100

    adds header file

--- landing_page_demo/container/src/App.js
+++ landing_page_demo/container/src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
-import MarketingApp from './components/MarketingApp';
+import { BrowserRouter } from 'react-router-dom';
 
+import Header from './components/Header';
+import MarketingApp from './components/MarketingApp';
 // Imports all the micro-projects into this main App file.
 
 export default () => {
-  return <MarketingApp />;
+  return (
+    <BrowserRouter>
+      <Header />
+      <MarketingApp />
+    </BrowserRouter>
+  );
 };
