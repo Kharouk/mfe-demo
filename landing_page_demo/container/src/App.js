e21e72ca4eb15dde6ec1ae6349fa5b1545e56aec
Alex Kharouk <akharoukpp@gmail.com>
2021-09-27 06:30:14 +0100

    78. FIX CLASSNAME COLLISION

--- landing_page_demo/container/src/App.js
+++ landing_page_demo/container/src/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { StylesProvider, createGenerateClassName } from '@material-ui/styles';
 
 import Header from './components/Header';
 import MarketingApp from './components/MarketingApp';
 // Imports all the micro-projects into this main App file.
 
+// generates more unique classnames so that we don't have collisions with MUI
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'cont',
+});
+
 export default () => {
   return (
     <BrowserRouter>
-      <Header />
-      <MarketingApp />
+      <StylesProvider generateClassName={generateClassName}>
+        <Header />
+        <MarketingApp />
+      </StylesProvider>
     </BrowserRouter>
   );
 };
