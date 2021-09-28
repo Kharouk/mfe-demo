546a12596450a4fa02541f65742590591aebe115
Alex Kharouk <akharoukpp@gmail.com>
2021-09-28 16:03:30 +0100

    adds custom webpack.common file for Vue

---
+++ landing_page_demo/dashboard/config/webpack.dev.js
@@ -0,0 +1,35 @@
+const { merge } = require('webpack-merge'); // merges webpack files
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const commonConfig = require('./webpack.common');
+const packageJson = require('../package.json');
+
+const PORT = 8082;
+
+const devConfig = {
+  mode: 'development',
+  output: {
+    publicPath: `http://localhost:${PORT}/`,
+  },
+  devServer: {
+    port: PORT,
+    historyApiFallback: {
+      index: '/index.html', // deals with routing in app
+    },
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'auth',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './App': './src/bootstrap',
+      },
+      shared: packageJson.dependencies,
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+};
+
+module.exports = merge(commonConfig, devConfig); // merging the two configs, with devConfig overriding
---
+++ landing_page_demo/dashboard/src/bootstrap.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createBrowserHistory, createMemoryHistory } from 'history';
+import App from './App';
+
+// Mount function to start up app:
+const mount = (el, { onSignIn, onNavigate, defaultHistory, initialPath }) => {
+  const history =
+    defaultHistory ||
+    createMemoryHistory({
+      initialEntries: [initialPath],
+    }); // if local dev, use default BROWSER history
+
+  // have history call onNavigate whenever called
+  if (onNavigate) history.listen(onNavigate);
+
+  ReactDOM.render(<App history={history} onSignIn={onSignIn} />, el);
+
+  // return this object with onParentNavigate function so we let auth know container has changed paths
+  return {
+    onParentNavigate({ pathname: nextPathname }) {
+      const { pathname } = history.location;
+      if (nextPathname !== pathname) history.push(nextPathname);
+    },
+  };
+};
+
+// Dev or isolation, call mount immediately:
+if (process.env.NODE_ENV === 'development') {
+  const devRoot = document.querySelector('#_auth-root');
+  if (devRoot) mount(devRoot, { defaultHistory: createBrowserHistory() });
+}
+
+// we are running through container so export mount:
+export { mount };
