384390f56efb3049f69d753bd293570859be1339
Alex Kharouk <akharoukpp@gmail.com>
2021-09-28 05:34:14 +0100

    Push to history /pricing when navigating to in marketing

--- landing_page_demo/container/src/components/MarketingApp.js
+++ landing_page_demo/container/src/components/MarketingApp.js
@@ -1,13 +1,16 @@
 import React, { useRef, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import { mount } from 'marketing/App';
 
 export default () => {
   const ref = useRef(null);
+  const history = useHistory();
 
   useEffect(() => {
     mount(ref.current, {
-      onNavigate: () => {
-        console.log('should be navigated.');
+      onNavigate: ({ pathname: nextPathname }) => {
+        const { pathname } = history.location;
+        if (pathname !== nextPathname) history.push(nextPathname);
       },
     });
   }, []);
