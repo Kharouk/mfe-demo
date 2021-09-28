0cc2a0ecbb27ea65ab3ec27468f3b7539213566e
Alex Kharouk <akharoukpp@gmail.com>
2021-09-28 05:28:18 +0100

    87. Pass onNavigate to Marketing component to begin syncing history obejct between host & remote

--- landing_page_demo/container/src/components/MarketingApp.js
+++ landing_page_demo/container/src/components/MarketingApp.js
@@ -5,7 +5,11 @@ export default () => {
   const ref = useRef(null);
 
   useEffect(() => {
-    mount(ref.current);
+    mount(ref.current, {
+      onNavigate: () => {
+        console.log('should be navigated.');
+      },
+    });
   }, []);
 
   return <div ref={ref} />;
