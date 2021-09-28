2f297495f6ac4af2740a1e8715f2d692e2ddcc00
Alex Kharouk <akharoukpp@gmail.com>
2021-09-28 09:22:05 +0100

    set up public path for marketing in case we need to add nested routes (fix 404 index.html issues as well)

--- landing_page_demo/marketing/config/webpack.dev.js
+++ landing_page_demo/marketing/config/webpack.dev.js
@@ -4,13 +4,17 @@ const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPl
 
 const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json');
+const PORT = 8081;
 
 const devConfig = {
   mode: 'development',
+  output: {
+    publicPath: `http://localhost:${PORT}/`,
+  },
   devServer: {
-    port: 8081,
+    port: PORT,
     historyApiFallback: {
-      index: 'index.html', // deals with routing in app
+      index: '/index.html', // deals with routing in app
     },
   },
   plugins: [
